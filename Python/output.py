140c5b5d757e80b261aaaa97aef2d286b305afe7
sho3019 <daisuke.syou.j8e@densotechno.com>
2022-11-28 16:21:56 +0900

    a

--- Python/output.py
+++ Python/output.py
@@ -5,7 +5,7 @@ json_open = open('../sample.json', 'r')
 json_load = json.load(json_open)
 
 if(calc.input.error_flag == 0):
-    json_load["calc"] == calc.ans
+    json_load["calc"] = calc.ans
 
     json_result = open('output.json', 'w')
     json.dump(json_load, json_result, indent = 4)
