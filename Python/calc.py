4647eb85c6664e7c3cfbe31341870a35dc878b27
sho3019 <daisuke.syou.j8e@densotechno.com>
2022-11-28 17:17:21 +0900

    AWS3

--- Python/calc.py
+++ Python/calc.py
@@ -15,7 +15,10 @@ if (input.error_flag == 0):
     elif(input.operation == "devision"):
         ans = input.num1 / input.num2
 
-    print("answer :" + str(ans))
+    print("Data1 : " + str(input.num1))
+    print("Data2 : " + str(input.num2))
+    print("type : " + input.operation)
+    print("answer : " + str(ans))
 
 else:
     ans = "error: incorrect input"
